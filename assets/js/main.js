abd50604f62d905653955a92fea8eb0c171be9b5
pradhanDebkanta <70876962+pradhanDebkanta@users.noreply.github.com>
2020-10-27 13:25:14 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,10 +85,10 @@
     $(document).ready(function(){
         $('body').bind('cut copy paste', function(e){
             e.preventDefault();
-        })
+        });
         $('body').on('contextmenu', function(e){
             return false;
-        })
+        });
     });
 
 })(jQuery);
